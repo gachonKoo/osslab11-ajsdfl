e52d20f9d6092fa55d282e36eecf7e5df3933336
ajsdfl <103660459+ajsdfl@users.noreply.github.com>
2025-11-14 16:43:56 +0900

    Add tester.py for geometry utility functions

---
+++ tester.py
@@ -0,0 +1,9 @@
+import geo.utils as utils [cite: 435]
+
+a, b = 3, 4 [cite: 442]
+c = utils.pythagoras(a, b)
+print('c', c) [cite: 449]
+
+r = 10 [cite: 460]
+area = utils.circle(r)
+print('area =', area) [cite: 466]
