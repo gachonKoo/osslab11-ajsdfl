b641cfab25e834cebba2eb916c43dc9875f1b099
ajsdfl <103660459+ajsdfl@users.noreply.github.com>
2025-11-14 17:05:59 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,21 +1,18 @@
 import sys
 import geo.utils as utils
 
-line1_raw = sys.stdin.readline() 
+line_raw = sys.stdin.readline()
 
-if line1_raw and len(line1_raw.split()) >= 2:
-    line1_parts = line1_raw.split()
-    a = int(line1_parts[0])
-    b = int(line1_parts[1])
+if line_raw:
+    parts = line_raw.split()
     
-    line2_raw = sys.stdin.readline()
-    
-    if line2_raw and len(line2_raw.split()) >= 1:
-        line2_parts = line2_raw.split()
-        r = int(line2_parts[0])
-
+    if len(parts) == 2:
+        a = int(parts[0])
+        b = int(parts[1])
         c = utils.pythagoras(a, b)
         print('c', c)
         
+    elif len(parts) == 1:
+        r = int(parts[0])
         area = utils.circle(r)
         print('area =', area)
